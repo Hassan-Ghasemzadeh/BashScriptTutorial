6eeb1ed2e8f9f0fb56ad7913c9752b58b3a11751
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-17 10:02:03 +0330

    [Feat] : just complete the variable

--- Variable/variable.sh
+++ Variable/variable.sh
@@ -5,3 +5,7 @@ myage="27"
 
 echo "My name is $myname"
 echo "My age is $myage"
+
+echo "my username in fedora is $USER"
+echo "my home directory is $HOME"
+echo "and my files are $(ls) and stored in $PWD"
