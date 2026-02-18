bb0f1433c59727d5232825d293822bdcb0f0c610
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-18 10:18:01 +0330

    [Feat] : learned file/directory existence checking

--- IfStatement/fileexists.sh
+++ IfStatement/fileexists.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# -e (Exists) -f (Regular File) -d (Directory)
+
+if [ -f ~/myfile.txt ]
+then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi
+
+if [ -d ~/VscodeProject ]
+then
+    echo "Directory exists"
+else
+    echo "Directory does not exist"
+fi
+
+if [ -e ~/m.txt ]
+then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi
