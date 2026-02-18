51512191898b0b80a0e150a4675f238adf337780
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-18 13:51:44 +0330

    [Docs]

--- exitcode/exitcode.sh
+++ exitcode/exitcode.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Redirection: > /dev/null 2>&1 
+# Suppresses all output (both standard output and error messages).
+# It ensures the script remains silent and only shows our custom echo messages.
+
 directory=/notexists
 
 if [ -d "$directory" ] > /dev/null 2>&1
