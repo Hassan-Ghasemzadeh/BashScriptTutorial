c4b71b82e43c51b72511276171c271f61d954607
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-18 10:06:53 +0330

    [Feat] : simple if statment completed

--- IfStatement/ifstatement.sh
+++ IfStatement/ifstatement.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# This script demonstrates the use of an if statement to check if a variable equals a specific value.
+# you can use -eq to check if is equal to a value, -ne for not equal, -gt for greater than, -lt for less than,
+# -ge for greater than or equal to, and -le for less than or equal to.
+
 number=250
-if [ $number -eq 250 ]
+if [ "$number" -eq 250 ] 
 then
     echo "The condition is true"
+else
+    echo "Variable is not equal to 250"
 fi
