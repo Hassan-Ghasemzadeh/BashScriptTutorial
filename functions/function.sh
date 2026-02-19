61e09be06f75b850ac9adb912a143ae0f779c663
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-19 12:35:47 +0330

    [Feat] : function added to update script

--- functions/function.sh
+++ functions/function.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+release_file=/etc/os-release
+log_file=/var/log/updater.log
+error_log=/var/log/updater_errors.log
+
+check_exit_status(){
+    if [ $? -ne 0 ]
+    then
+        echo "An error accured. please check $error_log file"
+    fi
+}
+
+if grep -q "Arch" $release_file
+then
+    #host is based on arch linux.
+    sudo packman -Syu 1>>$log_file 2>>$error_log
+    check_exit_status
+fi
+if grep -q "Pop" $release_file || grep -q "Ubuntu" $release_file
+then
+    #the host is based on ubuntu
+    #run the apt version of the command
+    sudo apt update 1>>$log_file 2>>$error_log
+    check_exit_status
+    sudo apt dist-upgrade -y 1>>$log_file 2>>$error_log
+    check_exit_status
+fi
