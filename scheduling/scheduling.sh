4061ea582f4999999d03ac554054f6188e22241d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-19 16:23:49 +0330

    [Feat]

--- scheduling/scheduling.sh
+++ scheduling/scheduling.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# you should use <<at>> for scheduling one time jobs
+# you can see your jobs by using <<atq>>
+# you can remove your job by using <<atrm jobId>> 
+
 $log_file=job_result.log
 
 echo "you will see this in that file at $(date)" > $log_file 
