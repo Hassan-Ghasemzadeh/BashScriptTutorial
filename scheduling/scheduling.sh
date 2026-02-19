2d773d1534f13601490051cc1ef7cc6b26857dbe
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-19 16:44:10 +0330

    [Feat] : crontab explantion added

--- scheduling/scheduling.sh
+++ scheduling/scheduling.sh
@@ -2,8 +2,24 @@
 
 
 # you should use <<at>> for scheduling one time jobs
+
 # you can see your jobs by using <<atq>>
+
 # you can remove your job by using <<atrm jobId>> 
+
+#crontab -e: Edit your crontab file (opens your default text editor).
+
+#crontab -l: List all your currently active scheduled tasks.
+
+#crontab -r: Remove all your current cron jobs (use with caution!).
+#Minute: The exact minute the command runs 0 - 59
+#Hour: The hour of the day (24-hour clock)0 - 23 
+#Day of Month: The day of the month 1 - 31
+#Month: The month of the year 1 - 12 (or Jan-Dec)
+#Day of Week: The day of the week 0 - 6 (Sunday is 0 or 7)
+# command: m h dom moy dow
+
+#crontab will schedule the job multiple times unless you cancel it
 
 $log_file=job_result.log
 
