8a9a0658dcb4a107bd99b9e44e464b3f4cc28ab7
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-19 13:09:13 +0330

    [Feat] : case done

--- case/case.sh
+++ case/case.sh
@@ -1,1 +1,40 @@
-#!/bin/bash/
+#!/bin/bash
+
+echo "What is your Linux distribution?"
+
+echo "1-Fedora"
+echo "2-Ubuntu"
+echo "3-Centos"
+echo "4-Kali"
+echo "5-Something else..."
+
+read distro
+
+case $distro in
+1)
+    #Fedora
+    sudo dnf update
+    sudo dnf upgrade
+    ;;
+2)
+    #Ubuntu
+    sudo apt update
+    sudo apt upgrade
+    ;;
+3)
+    #Centos
+    sudo yum update
+    sudo yum upgrade
+    ;;
+4)  
+    #Kali
+    sudo apt update
+    sudo apt upgrade
+    ;;
+*)
+    echo "Unknown Linux distribution"
+    echo "Please enter valid number"
+    exit 1;
+    ;;
+
+esac
