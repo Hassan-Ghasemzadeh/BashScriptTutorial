a42025f4cf0b65eb3079189d04bb30e66a1a95c4
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-20 11:38:48 +0330

    [Feat] : this will find number of files in a directory

--- arguments/lines.sh
+++ arguments/lines.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+lines=$(ls -lh | wc -l)
+if[ $# -ne 1 ]
+then
+    echo "This script requires exactly one directory path passed to it."
+    echo "Please try again"
+    exit 1
+fi
+
+echo "You have $($lines-1) object in $1 directory"
