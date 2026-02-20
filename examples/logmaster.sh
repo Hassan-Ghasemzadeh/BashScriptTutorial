74068c2b0007a05b60f755884d43322c5d6949a0
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-20 17:34:58 +0330

    [Feat] : initial setting done

--- examples/logmaster.sh
+++ examples/logmaster.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+#setting
+THRESHOLD=80
+LOG_DIR="/var/log/logmaster"
+BACKUP_DIR="/tmp/archive_logs"
+REPORT_FILE="/var/log/cleanup_report.log"
+
+#creates backup directory if not exists
+mkdir -p "$BACKUP_DIR"
