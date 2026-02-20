0d270f03bdb9e7de428ccc78d9a3886cd3f14d3e
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-20 17:44:46 +0330

    [Feat] : if statement done

--- examples/logmaster.sh
+++ examples/logmaster.sh
@@ -10,3 +10,14 @@ REPORT_FILE="/var/log/cleanup_report.log"
 mkdir -p "$BACKUP_DIR"
 
 echo "Start of maintenance operations in $(date)" >> "$REPORT_FILE"
+
+current_usage=$( df / | grep / | awk "${print $5}" / sed "s/%//" )
+if [ $current_usage -gt $THRESHOLD ]
+then
+
+
+
+else
+    echo "Disk status is normal ($CURRENT_USAGE%). No cleanup needed." >> "$REPORT_FILE"
+fi
+echo "maintenance done..." >> "$REPORT_FILE"
