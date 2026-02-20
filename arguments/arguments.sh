cc972823ece9e5a132cc13010b0146c527c3df69
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-20 11:34:14 +0330

    [Feat] : arguments fully explained

--- arguments/arguments.sh
+++ arguments/arguments.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# $# shows number arguments that we had sent
+# $@ shows all argument sepratly
+# $? last command result
+# ?* shows all argument in one string
+# $$ PID for running command 
+
+echo "You entered argument: $1"
