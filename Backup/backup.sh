896a60f3f35dac8d13ea1f864a961396afd79854
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-20 12:18:32 +0330

    [Feat] : check that two argument has entered added

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# check to make sure that user has entered exactly two argument
+if [ $# -ne 2 ]
+then
+    echo "Usage: backup.sh <source_directory> <target_directory>"
+    echo "Please try again"
+    exit 1
+fi
