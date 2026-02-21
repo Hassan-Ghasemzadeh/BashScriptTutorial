1bcce40f598362b464c68797732a85a21da1a1f0
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-21 12:55:15 +0330

    [Feat] : check if auth log file is available

--- examples/authgaurd.sh
+++ examples/authgaurd.sh
@@ -4,3 +4,13 @@
 auth_log="/var/log/secure"
 threshold=5
 report="/tmp/failed_login_report.txt"
+
+echo "Security check report ($(date))" >> "$report"
+
+if[[ ! -f "$auth_log" ]]
+then
+    echo "$auth_log is unavailable.Do you have root access?"
+    exit 1
+fi
+
+
