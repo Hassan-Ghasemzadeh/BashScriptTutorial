3dd14c927222d079dce2c6cd93d8100f9891a67a
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-21 13:09:27 +0330

    [Feat] : number of failed attempt found

--- examples/authgaurd.sh
+++ examples/authgaurd.sh
@@ -13,4 +13,13 @@ then
     exit 1
 fi
 
+grep "Failed password" "$auth_log" | \
+awk '{
+    if(i=1;i<=NF;i++){
+        if($i ~ /^[0-9]+\.[0-9]+\.+[0-9]\.+[0-9]+$/) print $i
+    }
+}
+' | \
+sort | uniq -c | sort -nr | \
+awk -v limit="$threshold" '$i > limit { print "IP: " $2 " | Number of failed try $1 }' >> "$report"
 
