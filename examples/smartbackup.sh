72544c380994f7b1fd06f6ce22a15b5ea4def05b
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-22 14:05:50 +0330

    [Feat] : initial env variable imported

--- examples/smartbackup.sh
+++ examples/smartbackup.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+source_dir="/user/home/my_project"
+backup_path="/tmp/backups"
+remote_user="admin"
+remote_host="192.168.1.50"
+remote_dir="/backup/storage"
+timestamp="$(date +%Y-%m-%d %H:%M:S)"
+backup_name="project_backup_$timestamp.tar.gz"
+
