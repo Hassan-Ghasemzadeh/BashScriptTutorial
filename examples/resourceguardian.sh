70235936b607278ae6fd8d83d277da5cd5a6c10d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-23 14:59:51 +0330

    [Feat] : explaination added

--- examples/resourceguardian.sh
+++ examples/resourceguardian.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+
+# -e: (errexit) Exit immediately if a command exits with a non-zero status.
+# -u: (nounset) Treat unset variables as an error when substituting.
+# -o pipefail: The return value of a pipeline is the status 
+# of the last command to exit with a non-zero status, or zero if no command exited with a non-zero status.
 set -euo pipefail
