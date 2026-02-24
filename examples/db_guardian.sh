b4735706784dc20dbe6fb3aa3923497ec10c5d52
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2026-02-24 20:10:52 +0330

    [Feat] : usage function added

--- examples/db_guardian.sh
+++ examples/db_guardian.sh
@@ -4,3 +4,9 @@ readonly BACKUP_DIR="/var/backup/db_backups"
 readonly LOG_FILE="/var/log/db_guardian.log"
 DB_NAME=""
 DB_USER="Root"
+
+usage(){
+    echo "Usage: $0 -d <database_name> [-u <database_user>]"
+    echo "-d: (required) Name of the database to backup "
+    echo "-u: (optional) Database user (default:Root)"
+}
